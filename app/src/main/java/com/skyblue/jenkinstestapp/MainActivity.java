c8615931b3a34a208b1fd29653f72c5082686b0f
Prasanth <prasanthcena968@gmail.com>
2024-03-27 15:53:38 +0530

    initial commit

---
+++ app/src/main/java/com/skyblue/jenkinstestapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.skyblue.jenkinstestapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
